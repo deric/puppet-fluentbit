5ed05a53883fedb1ed43761e8d1940fda9ae2e4f
Tomas Barton <barton.tomas@gmail.com>
2023-07-19 23:08:18 +0200

    Basic specs

--- spec/classes/fluentbit_spec.rb
+++ spec/classes/fluentbit_spec.rb
@@ -4,10 +4,9 @@ require 'spec_helper'
 
 describe 'fluentbit' do
   _, os_facts = on_supported_os.first
+  let(:facts) { os_facts }
 
   context 'with default parameters' do
-    let(:facts) { os_facts }
-
     it { is_expected.to compile.with_all_deps }
 
     it { is_expected.to contain_class('fluentbit') }
@@ -17,5 +16,59 @@ describe 'fluentbit' do
     it { is_expected.to contain_class('fluentbit::service') }
     it { is_expected.to contain_package('fluent-bit').with_ensure(%r{present|installed}) }
     it { is_expected.to contain_service('fluentbit').with_ensure('running') }
+
+    it { is_expected.to contain_concat('/etc/fluent-bit/pipelines/inputs.conf') }
+    it { is_expected.to contain_concat('/etc/fluent-bit/pipelines/outputs.conf') }
+    it { is_expected.to contain_concat('/etc/fluent-bit/pipelines/filters.conf') }
+
+    it {
+      is_expected.to contain_file('/etc/fluent-bit').with(
+        ensure: 'directory',
+      )
+    }
+
+    it {
+      is_expected.to contain_file('/etc/fluent-bit/fluent-bit.conf').with(
+        ensure: 'file',
+      )
+    }
+  end
+
+  context 'with custom directories' do
+    let(:params) do
+      {
+        config_dir: '/etc/fluentbit',
+        config_file: '/etc/fluentbit/fluent-bit.conf',
+        plugin_dir: '/etc/fluentbit/pipelines',
+        scripts_dir: '/etc/fluentbit/lua-scripts'
+      }
+    end
+
+    it { is_expected.to compile.with_all_deps }
+
+    it {
+      is_expected.to contain_file('/etc/fluentbit').with(
+        ensure: 'directory',
+        purge: true,
+        recurse: true,
+      )
+    }
+
+    it {
+      is_expected.to contain_file('/etc/fluentbit/pipelines').with(
+        ensure: 'directory',
+        purge: true,
+        recurse: true,
+      )
+    }
+
+    it {
+      is_expected.to contain_file('/etc/fluentbit/lua-scripts').with(
+        ensure: 'directory',
+        purge: true,
+        recurse: true,
+      )
+    }
+
   end
 end
